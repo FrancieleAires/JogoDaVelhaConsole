b99c2fb7edee31321abad691e62a9357a9b4eeae
FrancieleAires <f.aires2016@gmail.com>
2023-01-20 13:49:32 -0300

    projeto-jogo-da-velha

---
+++ JogoDaVelha.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaVelha
+{
+    internal class JogoDaVelha
+    {
+        
+        private bool fimDeJogo;
+        private char[] posicoes;
+        private char vez;
+        private int quantidadePreenchida;
+
+        public JogoDaVelha ()
+        {
+            fimDeJogo = false;
+            posicoes = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            vez = 'x';
+            quantidadePreenchida = 0;
+        }
+        public void Iniciar()
+        {
+            while (!fimDeJogo)
+            {
+                RenderizarTabela();
+                LerEscolhaDoUsuário();
+                RenderizarTabela();
+                VerificarFimDeJogo();
+                MudarVez();
+            }
+        }
+
+        private void MudarVez()
+        {
+            vez = vez == 'x' ? 'O' : 'x';
+        }
+
+        private void VerificarFimDeJogo()
+        {
+            if (quantidadePreenchida < 5)
+                return;
+
+            if (ExisteVitoriaHorizontal() || ExisteVitoriaVertical() || ExisteVitoriaDiagonal())
+            {
+                fimDeJogo = true;
+                Console.WriteLine($"Fim de jogo! Vitória de {vez}");
+                return;
+            }
+            if(quantidadePreenchida is 9)
+            {
+                fimDeJogo = true;
+                Console.WriteLine("Fim de jogo! EMPATE");
+            }
+        }
+
+        private bool ExisteVitoriaHorizontal ()
+        {
+            bool vitoriaLinha1 = posicoes[0] == posicoes[1] && posicoes[0] == posicoes[2];
+            bool vitoriaLinha2 = posicoes[3] == posicoes[4] && posicoes[3] == posicoes[5];
+            bool vitoriaLinha3 = posicoes[6] == posicoes[7] && posicoes[6] == posicoes[8];
+
+            return vitoriaLinha1 || vitoriaLinha2 || vitoriaLinha3;
+        }
+
+
+        private bool ExisteVitoriaVertical()
+        {
+            bool vitoriaLinha1 = posicoes[0] == posicoes[3] && posicoes[0] == posicoes[6];
+            bool vitoriaLinha2 = posicoes[1] == posicoes[4] && posicoes[1] == posicoes[7];
+            bool vitoriaLinha3 = posicoes[2] == posicoes[5] && posicoes[2] == posicoes[8];
+
+            return vitoriaLinha1 || vitoriaLinha2 || vitoriaLinha3;
+
+        }
+
+        private bool ExisteVitoriaDiagonal()
+        {
+            bool vitoriaLinha1 = posicoes[2] == posicoes[4] && posicoes[2] == posicoes[6];
+            bool vitoriaLinha2 = posicoes[0] == posicoes[4] && posicoes[0] == posicoes[8];
+            return vitoriaLinha1 || vitoriaLinha2;
+
+        }
+
+        private void LerEscolhaDoUsuário()
+        {
+            Console.WriteLine($"Agora é a vez de {vez}, entre uma posição de 1 a 9 disponível");
+            bool conversao = int.TryParse(Console.ReadLine(), out int posicaoEscolhida);
+           
+            while (!conversao || !ValidarEscolhaUsuario(posicaoEscolhida))
+            {
+                Console.WriteLine("Sua escolha é inválida, por favor digite um número entre 1 e 9 disponível na tabela.");
+                conversao = int.TryParse(Console.ReadLine(), out  posicaoEscolhida);
+            }
+
+            PreencherEscolha(posicaoEscolhida);
+        }
+
+        private void PreencherEscolha(int posicaoEscolhida)
+        {
+            int indice = posicaoEscolhida - 1;
+            posicoes[indice] = vez;
+            quantidadePreenchida++;
+        }
+
+        private bool ValidarEscolhaUsuario(int posicaoEscolhida)
+        {
+            int indice = posicaoEscolhida - 1;
+
+            return posicoes[indice] != 'O' && posicoes[indice] != 'x';
+
+            
+        }
+
+        private void RenderizarTabela()
+        {
+            Console.Clear();
+            Console.WriteLine(ObterTabela());
+        }
+
+        private string ObterTabela()
+        {
+
+            return $"__{posicoes[0]}__|__{posicoes[1]}__|__{posicoes[2]}__\n" +
+                   $"__{posicoes[3]}__|__{posicoes[4]}__|__{posicoes[5]}__\n" +
+                   $"  {posicoes[6]}  |  {posicoes[7]}  |  {posicoes[8]}  \n\n";
+        }
+    }
+}
